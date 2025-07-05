afd114e7ab90b078b489e5d527593c18678e6332
William Ramirez <william.ramirez.paucar@udla.edu.ec>
2025-07-05 14:17:36 -0500

    Add annotations for sql lite table

--- Models/People.cs
+++ Models/People.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace PeopleApp.Models
 {
+    [Table("People")]
     internal class People
     {
+        [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
+        [MaxLength(250), Unique]
         public string Name { get; set; }
     }
 }
